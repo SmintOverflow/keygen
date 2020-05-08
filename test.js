385c1cb42f2f99fdaa92f63fad8af86acb4a7c36
Smint <ssg.jss2018@gmail.com>
2020-05-08 14:19:08 +0530

    added version 2.0.0

--- test.js
+++ test.js
@@ -1,26 +1,26 @@
-const fs = require('fs')
-let openpgpKeygen = require("openpgp-keygen")
+var fs = require('fs')
+var keygen = require('./keygen.js')
 console.log("Reading Public Key")
-keygen.Keygen()
+keygen.Keygen('user@domain.com', 'user', '1024', 'password');
 setTimeout(() => {
     fs.readFile('./pkey.txt', (err, data) => {
         if (err) {
-            console.error(err)
-            return
+            console.error(err);
+            return;
         }
-        console.log(data)
-    })
+        console.log(data);
+    });
 }, 5000);
 
 setTimeout(() => {
-    console.log("Reading Private Key")
+    console.log("Reading Private Key");
 }, 7000);
 setTimeout(() => {
     fs.readFile('./skey.txt', (err, data) => {
         if (err) {
-            console.error(err)
-            return
+            console.error(err);
+            return;
         }
-        console.log(data)
-    })
+        console.log(data);
+    });
 }, 12000);
